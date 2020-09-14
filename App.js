a156c67058fc97e809d379d3900f09c1cce93425
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-09-15 00:06:18 +0200

    Use enableScreens to improve performance

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { StyleSheet, View } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 import MealsNavigator from "./navigation/MealsNavigator";
+import { enableScreens } from "react-native-screens";
+
+enableScreens();
 
 const fetchFonts = () => {
   Font.loadAsync({
