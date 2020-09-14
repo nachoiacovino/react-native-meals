161e9ef13243fdf51ebf3f81f993972badf67b52
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-09-15 00:39:15 +0200

    Add FlatList

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { MEALS } from "../assets/dummy-data";
 
 const CategoryMealsScreen = ({ navigation: { getParam } }) => {
   const category = getParam("category");
+  const displayedMeals = MEALS.filter((meal) =>
+    meal.categoryIds.includes(category.id)
+  );
+
+  console.log(displayedMeals);
 
   return (
     <View>
-      <Text>{category.title}</Text>
+      <FlatList
+        data={displayedMeals}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
